606ecb2c04f1fcb65db1d2b69ee47edcb7bf4332
Marina Nenova <marinanenova89@gmail.com>
2022-07-25 17:21:23 +0300

    Added logic for the Snake Game project

---
+++ projects/Snake_game/snake_game.py
@@ -0,0 +1,48 @@
+from turtle import Screen
+import time
+
+from projects.Snake_game.food import Food
+from score_board import ScoreBoard
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score_board = ScoreBoard()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score_board.increase_score()
+        snake.extend()
+
+    # detect collision with a wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score_board.game_over()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score_board.game_over()
+
+screen.exitonclick()

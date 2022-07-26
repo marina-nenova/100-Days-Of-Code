deca8f0873deeb6535ed4dade94590a678b927e0
Marina Nenova <marinanenova89@gmail.com>
2022-07-26 13:05:38 +0300

    Added logic to detect ball collision with paddles

--- projects/Pong_game/main.py
+++ projects/Pong_game/main.py
@@ -29,7 +29,11 @@ while game_is_on:
 
     # detect collision with a wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < - 320:
+        ball.bounce_x()
 
 
 

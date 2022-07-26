f4442947b168801925f5e8a6dd7675502165dc2e
Marina Nenova <marinanenova89@gmail.com>
2022-07-26 13:27:42 +0300

    Added logic to show the score board

--- projects/Pong_game/main.py
+++ projects/Pong_game/main.py
@@ -4,11 +4,14 @@ from projects.Pong_game.ball import Ball
 from projects.Pong_game.paddle import Paddle
 import time
 
+from score_board import ScoreBoard
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
+score_board = ScoreBoard()
 
 ball = Ball()
 r_paddle = Paddle((350, 0))
@@ -23,7 +26,7 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -35,6 +38,14 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < - 320:
         ball.bounce_x()
 
+    #detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score_board.l_point()
 
+    # detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score_board.r_point()
 
 screen.exitonclick()

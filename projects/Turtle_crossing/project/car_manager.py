b73efa9c85f6b22ce7af1b8d1a64eceb99ac3ec0
Marina Nenova <marinanenova89@gmail.com>
2022-07-27 12:58:10 +0300

    Added logic to detect turtle collision with a car.

--- projects/Turtle_crossing/project/car_manager.py
+++ projects/Turtle_crossing/project/car_manager.py
@@ -3,6 +3,7 @@ import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+RANDOM_Y = list(range(-250, 250, 30))
 
 
 class CarManager(Turtle):
@@ -17,7 +18,7 @@ class CarManager(Turtle):
             new_car.shapesize(stretch_wid=1, stretch_len=2)
             new_car.penup()
             new_car.color(random.choice(COLORS))
-            random_y = random.randint(-250, 250)
+            random_y = random.choice(RANDOM_Y)
             new_car.goto(300, random_y)
             new_car.setheading(180)
             self.all_cars.append(new_car)

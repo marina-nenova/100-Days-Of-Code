13fe6bb76105a7d3f3e36fcddc90c9b9e73ff2bf
Marina Nenova <marinanenova89@gmail.com>
2022-07-27 13:08:21 +0300

    Added logic to detect turtle reaching the finish line

--- projects/Turtle_crossing/project/car_manager.py
+++ projects/Turtle_crossing/project/car_manager.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+MOVE_INCREMENT = 5
 RANDOM_Y = list(range(-250, 250, 30))
 
 
@@ -10,6 +10,7 @@ class CarManager(Turtle):
     def __init__(self):
         super().__init__()
         self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_car(self):
         random_chance = random.randint(1, 6)
@@ -25,4 +26,7 @@ class CarManager(Turtle):
 
     def move_cars(self):
         for car in self.all_cars:
-            car.forward(STARTING_MOVE_DISTANCE)
+            car.forward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
--- projects/Turtle_crossing/project/scoreboard.py
+++ projects/Turtle_crossing/project/scoreboard.py
@@ -1,5 +1,18 @@
-FONT = ("Courier", 24, "normal")
+# from turtle import Turtle
+#
+# FONT = ("Courier", 24, "normal")
+#
+#
+# class Scoreboard(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.color("white")
+#         self.penup()
+#         self.hideturtle()
+#         self.level = 0
+#         self.update_scoreboard()
+#
+#     def update_scoreboard(self):
+#         pass
 
 
-class Scoreboard:
-    pass

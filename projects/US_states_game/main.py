8f7c002d8a79e9abfc525edae43b2b8cf4d2a24a
Marina Nenova <marinanenova89@gmail.com>
2022-07-30 16:26:03 +0300

    Added logic for when the player guesses all 50 states correctly

--- projects/US_states_game/main.py
+++ projects/US_states_game/main.py
@@ -1,26 +1,22 @@
 import turtle
 import pandas
 
-
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 img = "blank_states_img.gif"
 screen.addshape(img)
 turtle.shape(img)
 
-
 data = pandas.read_csv("50_states.csv")
 all_states = data['state'].to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct",
+                                    prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("States to learn.csv")
         break
@@ -33,6 +29,9 @@ while len(guessed_states) < 50:
         t.write(answer_state)
         if answer_state not in guessed_states:
             guessed_states.append(answer_state)
+        if len(guessed_states) == 50:
+            t.goto(10, 240)
+            t.write("CONGRATULATIONS! YOU GOT THEM ALL!", align="center", font=("Courier", 20, "normal"))
+            break
 
-
-
+screen.exitonclick()
